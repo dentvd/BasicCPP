2b872bcda4f1505803f8d60956189b9ae2fcaf48
dentvd <deniskovalchuk79@gmail.com>
2022-10-12 16:40:07 +0300

    ex 1

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Name\t\t" << ":\t" << "\"The war and the peace\"" << endl;
+	cout << "Avtor\t\t" << ":\t" << "L.N. Tolstoj" << endl;
+	cout << "Izdatelstvo \t" << ":\t" << "Piter" << endl;
+	cout << "Pages \t\t" << ":\t" << "500" << endl;
+	return 0;
+}
