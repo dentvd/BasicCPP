2c124abae0e75af0627df10219ba783a642d377b
dentvd <deniskovalchuk79@gmail.com>
2022-10-12 16:57:25 +0300

    ex 3

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "\n" << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)187 << endl;
+	cout << "\n" << (char)186 << "\t\t\tPory Roku \t\t\t\t" << (char)186 << endl;
+	cout << "\n" << (char)204 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)185 << endl;
+	cout << "\n" << (char)186 << "\tZyma\t" << (char)186 << "\tVesna\t" << (char)186 << "\tLito\t" << (char)186 << "\tOsin\t" << (char)186 << endl;
+	cout << "\n" << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)188 << endl;
+	return 0;
+}
