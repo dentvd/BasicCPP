7f19cd1b4e6dc7cdebf1a9c081a550d6b5b0651f
dentvd <deniskovalchuk79@gmail.com>
2022-10-12 16:41:48 +0300

    ex 2

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "\"\a\a\aKraj lukomor’a dub zelenyj, " << endl;
+	cout << "\a\a\aI zolotyj lancyuh na nim:" << endl;
+	cout << "\a\a\aShodnya, shonochi kit uchenyj" << endl;
+	cout << "\a\a\aNa lancyuhu kruzhl’aje tim\"" << endl;
+	return 0;
+}
